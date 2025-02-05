bb1f0411935f86181998db05176ef2b534b59228
Mohsen-Esfandyari <Mohsen.e.doulabi@gmail.com>
2025-02-05 00:33:58 -0500

    Pylint Error solved

--- rand.py
+++ rand.py
@@ -6,13 +6,13 @@ import secrets
 
 def random_array(size=None, arr=None, min_val=1, max_val=20, secure=False):
     """Generates a random array with specified parameters."""
-    rand_func = secrets.randbelow if secure else lambda x: secrets.randbelow(x)
+    rand_func = secrets.randbelow
     if arr is not None:
         for i, _ in enumerate(arr):
             arr[i] = rand_func(max_val) + (1 if secure else 0)
         return arr
     if size is not None:
-        return [rand_func(max_val) + (1 if secure else 0) for _ in range(size)]
+        return [rand_func(max_val- min_val) + (1 if secure else 0) for _ in range(size)]
     raise ValueError("Either arr or size must be provided.")
 
 random_array_instance = random_array(20, secure=True)  # Uses secure randomness
