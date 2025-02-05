fd9f740adfdc629ee3cda662d2b46f0085356f56
Ali Farahat <ali.farahat78@gmail.com>
2025-02-04 21:36:00 -0500

    Add tests and update hw2_debugging.py

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,32 +1,32 @@
 import rand
 
 def mergeSort(arr):
-    if (len(arr) == 1):
+    if len(arr) <= 1:
         return arr
 
-    half = len(arr)//2
+    half = len(arr) // 2
+    left = mergeSort(arr[:half])
+    right = mergeSort(arr[half:])
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    return recombine(left, right)
 
 def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
+    leftIndex, rightIndex = 0, 0
+    mergeArr = []
+
     while leftIndex < len(leftArr) and rightIndex < len(rightArr):
         if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-        else:
+            mergeArr.append(leftArr[leftIndex])
             leftIndex += 1
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+        else:
+            mergeArr.append(rightArr[rightIndex])
+            rightIndex += 1
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
+    mergeArr.extend(leftArr[leftIndex:])
+    mergeArr.extend(rightArr[rightIndex:])
     
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
-
     return mergeArr
+
 
 arr = rand.random_array([None] * 20)
 arr_out = mergeSort(arr)
--- rand.py
+++ rand.py
@@ -1,8 +1,6 @@
-import subprocess
+import random
 
 def random_array(arr):
-    shuffled_num = None
     for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+        arr[i] = random.randint(1, 20)  # Generates random numbers between 1 and 20
     return arr
